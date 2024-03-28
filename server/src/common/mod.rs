06d884cc4220761d8cf0dd648bebefe5391efda2
A.M. Smith <smitham42@gmail.com>
2024-03-28 05:00:57 -0700

    feat: add get plans interface

--- server/src/common/mod.rs
+++ server/src/common/mod.rs
@@ -31,34 +31,42 @@ impl fmt::Display for AckError {
 /// This request might come in over REST, or through GRPC someday
 ///  if another microservice has a software-hardware link to a radio antenna
 pub async fn ack_flight(fp_id: String, grpc_clients: &GrpcClients) -> Result<(), AckError> {
-    //
-    // TODO(R4) - Check that it came from authenticated source
-    //
+    let client = &grpc_clients.storage.flight_plan;
+
+    let result = client
+        .get_by_id(Id { id: fp_id.clone() })
+        .await
+        .map_err(|e| {
+            common_error!("(ack_flight) {}", e);
+            AckError::Internal
+        })?;
+
+    let mut data = result.into_inner().data.ok_or_else(|| {
+        common_error!("(ack_flight) Couldn't get data from object id: {}", fp_id);
+        AckError::Internal
+    })?;
+
+    data.carrier_ack = Some(Utc::now().into());
 
     //
     // Update the flight plan record to show that it has been acknowledged
     //
     let request = flight_plan::UpdateObject {
         id: fp_id,
-        data: Some(flight_plan::Data {
-            carrier_ack: Some(Utc::now().into()),
-            ..Default::default()
-        }),
+        data: Some(data),
         mask: Some(FieldMask {
             paths: vec!["carrier_ack".to_string()],
         }),
     };
 
-    let client = &grpc_clients.storage.flight_plan;
-
     //
     // TODO(R4) - Push to queue and retry on failure
     //
-    let Ok(_response) = client.update(request).await else {
+    client.update(request).await.map_err(|e| {
         let error_msg = "svc-storage failure.".to_string();
-        common_error!("(acknowledge_flight_plan) {}", &error_msg);
-        return Err(AckError::Internal);
-    };
+        common_error!("(acknowledge_flight_plan) {}: {e}", &error_msg);
+        AckError::Internal
+    })?;
 
     Ok(())
 }
--- server/src/rest/api.rs
+++ server/src/rest/api.rs
@@ -7,7 +7,7 @@ pub mod rest_types {
 pub use rest_types::*;
 
 use crate::grpc::client::GrpcClients;
-use axum::{extract::Extension, Json};
+use axum::{body::Bytes, extract::Extension, Json};
 use hyper::StatusCode;
 
 use svc_storage_client_grpc::prelude::*;
@@ -56,10 +56,84 @@ pub async fn health_check(
     }
 }
 
+impl TryFrom<flight_plan::Object> for FlightPlan {
+    type Error = StatusCode;
+
+    fn try_from(object: flight_plan::Object) -> Result<Self, Self::Error> {
+        let flight_uuid = object.id;
+        let data = object.data.ok_or_else(|| {
+            rest_error!("(try_from) could not get data from object.");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+        let origin_vertiport_id = data.origin_vertiport_id.ok_or_else(|| {
+            rest_error!("(try_from) could not get origin_vertiport_id from data.");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+        let target_vertiport_id = data.target_vertiport_id.ok_or_else(|| {
+            rest_error!("(try_from) could not get target_vertiport_id from data.");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+        let origin_timeslot_start = data.origin_timeslot_start.ok_or_else(|| {
+            rest_error!("(try_from) could not get origin_timeslot_start from data.");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+        let origin_timeslot_end = data.origin_timeslot_end.ok_or_else(|| {
+            rest_error!("(try_from) could not get origin_timeslot_end from data.");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+        let target_timeslot_start = data.target_timeslot_start.ok_or_else(|| {
+            rest_error!("(try_from) could not get target_timeslot_start from data.");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+        let target_timeslot_end = data.target_timeslot_end.ok_or_else(|| {
+            rest_error!("(try_from) could not get target_timeslot_end from data.");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+        let path = data.path.ok_or_else(|| {
+            rest_error!("(try_from) could not get path from data.");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+        let path = path
+            .points
+            .iter()
+            .map(|p| PointZ {
+                latitude: p.latitude,
+                longitude: p.longitude,
+                altitude_meters: p.altitude,
+            })
+            .collect();
+
+        Ok(FlightPlan {
+            session_id: data.session_id,
+            flight_uuid,
+            aircraft_id: data.vehicle_id,
+            origin_vertiport_id,
+            target_vertiport_id,
+            origin_vertipad_id: data.origin_vertipad_id,
+            target_vertipad_id: data.target_vertipad_id,
+            origin_timeslot_start: origin_timeslot_start.into(),
+            origin_timeslot_end: origin_timeslot_end.into(),
+            target_timeslot_start: target_timeslot_start.into(),
+            target_timeslot_end: target_timeslot_end.into(),
+            path,
+            acquire: vec![],
+            deliver: vec![],
+        })
+    }
+}
+
 /// Acknowledge a flight
 #[utoipa::path(
     post,
-    path = "/ack/flight",
+    path = "/atc/acknowledge",
     tag = "svc-atc",
     request_body = AckRequest,
     responses(
@@ -78,5 +152,373 @@ pub async fn acknowledge_flight_plan(
             rest_error!("(acknowledge_flight_plan) {}", e);
             Err(StatusCode::INTERNAL_SERVER_ERROR)
         }
+    }
+}
+
+/// Get flight plans
+#[utoipa::path(
+    get,
+    path = "/atc/plans",
+    tag = "svc-atc",
+    request_body = String,
+    responses(
+        (status = 200, description = "Request successful.", body = String),
+        (status = 500, description = "Request unsuccessful."),
+    )
+)]
+pub async fn get_flight_plans(
+    Extension(grpc_clients): Extension<GrpcClients>,
+    aircraft_id: Bytes,
+) -> Result<Json<Vec<FlightPlan>>, StatusCode> {
+    rest_debug!("(get_flight_plans) entry.");
+    let aircraft_id = String::from_utf8(aircraft_id.to_vec()).map_err(|_| {
+        rest_error!("(get_flight_plans) could not convert aircraft_id to string.");
+        StatusCode::BAD_REQUEST
+    })?;
+
+    let now = chrono::Utc::now();
+
+    // TODO(R5): parameterize duration lookahead?
+    let delta = chrono::Duration::try_minutes(60).ok_or_else(|| {
+        rest_error!("(get_flight_plans) could not create duration.");
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
+
+    //
+    // TODO(R5): Move this search to a loop searching for all
+    //  aircraft, and push results to local cache
+    // So that we can return cached results and not search svc-storage
+    //  every time an aircraft asks for flight plans
+    //
+    let filter = AdvancedSearchFilter::search_equals("vehicle_id".to_owned(), aircraft_id.clone())
+        .and_between(
+            "origin_timeslot_start".to_owned(),
+            (now - delta).to_string(),
+            (now + delta).to_string(),
+        );
+
+    let client = &grpc_clients.storage.flight_plan;
+    let mut plans = client
+        .search(filter)
+        .await
+        .map_err(|e| {
+            let error_msg = "svc-storage failure.".to_string();
+            rest_error!("(get_flight_plans) {}: {e}", &error_msg);
+
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?
+        .into_inner()
+        .list
+        .into_iter()
+        .filter_map(|object| FlightPlan::try_from(object).ok())
+        .collect::<Vec<FlightPlan>>();
+
+    // TODO(R6): Check cargo or rideshare
+    for plan in plans.iter_mut() {
+        let filter = AdvancedSearchFilter::search_equals(
+            "flight_plan_id".to_owned(),
+            plan.flight_uuid.clone(),
+        );
+
+        grpc_clients
+            .storage
+            .flight_plan_parcel
+            .search(filter)
+            .await
+            .map_err(|e| {
+                let error_msg = "svc-storage failure.".to_string();
+                rest_error!("(get_flight_plans) {}: {e}", &error_msg);
+
+                StatusCode::INTERNAL_SERVER_ERROR
+            })?
+            .into_inner()
+            .list
+            .into_iter()
+            .for_each(|parcel| {
+                let id = parcel.parcel_id;
+                if parcel.acquire {
+                    plan.acquire.push(Cargo {
+                        id: id.clone(),
+                        // weight_g: parcel.weight_g
+                    })
+                }
+
+                if parcel.deliver {
+                    plan.deliver.push(Cargo {
+                        id,
+                        // weight_g: parcel.weight_g
+                    })
+                }
+            });
+    }
+
+    rest_debug!("(get_flight_plans) returning {} plans.", plans.len());
+    Ok(Json(plans))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lib_common::uuid::Uuid;
+    use svc_storage_client_grpc::prelude::{GeoLineStringZ, GeoPointZ};
+
+    #[test]
+    fn test_from_flight_plan_object_valid() {
+        let now = Utc::now();
+        let object = flight_plan::Object {
+            id: "id".to_string(),
+            data: Some(flight_plan::Data {
+                session_id: "session_id".to_string(),
+                vehicle_id: "vehicle_id".to_string(),
+                origin_vertiport_id: Some("origin_vertiport_id".to_string()),
+                target_vertiport_id: Some("target_vertiport_id".to_string()),
+                origin_vertipad_id: "origin_vertipad_id".to_string(),
+                target_vertipad_id: "target_vertipad_id".to_string(),
+                origin_timeslot_start: Some((now + Duration::try_seconds(1).unwrap()).into()),
+                origin_timeslot_end: Some((now + Duration::try_seconds(2).unwrap()).into()),
+                target_timeslot_start: Some((now + Duration::try_seconds(3).unwrap()).into()),
+                target_timeslot_end: Some((now + Duration::try_seconds(4).unwrap()).into()),
+                path: Some(GeoLineStringZ {
+                    points: vec![GeoPointZ {
+                        y: 1.0,
+                        x: 2.0,
+                        z: 3.0,
+                    }],
+                }),
+                ..Default::default()
+            }),
+        };
+
+        let flight_plan = FlightPlan::try_from(object).unwrap();
+        assert_eq!(flight_plan.session_id, "session_id");
+        assert_eq!(flight_plan.flight_uuid, "id");
+        assert_eq!(flight_plan.aircraft_id, "vehicle_id");
+
+        assert_eq!(
+            flight_plan.origin_vertiport_id,
+            "origin_vertiport_id".to_string()
+        );
+        assert_eq!(
+            flight_plan.target_vertiport_id,
+            "target_vertiport_id".to_string()
+        );
+
+        assert_eq!(flight_plan.origin_vertipad_id, "origin_vertipad_id");
+        assert_eq!(flight_plan.target_vertipad_id, "target_vertipad_id");
+
+        assert_eq!(
+            flight_plan.origin_timeslot_start,
+            now + Duration::try_seconds(1).unwrap()
+        );
+        assert_eq!(
+            flight_plan.origin_timeslot_end,
+            now + Duration::try_seconds(2).unwrap()
+        );
+        assert_eq!(
+            flight_plan.target_timeslot_start,
+            now + Duration::try_seconds(3).unwrap()
+        );
+        assert_eq!(
+            flight_plan.target_timeslot_end,
+            now + Duration::try_seconds(4).unwrap()
+        );
+
+        let path = flight_plan.path;
+        assert_eq!(path.len(), 1);
+        assert_eq!(path[0].latitude, 1.0);
+        assert_eq!(path[0].longitude, 2.0);
+        assert_eq!(path[0].altitude_meters, 3.0);
+
+        assert!(flight_plan.acquire.is_empty());
+        assert!(flight_plan.deliver.is_empty());
+    }
+
+    #[test]
+    fn test_from_flight_plan_object_invalid() {
+        let now = Utc::now();
+        let object = flight_plan::Object {
+            id: "id".to_string(),
+            data: Some(flight_plan::Data {
+                session_id: "session_id".to_string(),
+                vehicle_id: "vehicle_id".to_string(),
+                origin_vertiport_id: Some("origin_vertiport_id".to_string()),
+                target_vertiport_id: Some("target_vertiport_id".to_string()),
+                origin_vertipad_id: "origin_vertipad_id".to_string(),
+                target_vertipad_id: "target_vertipad_id".to_string(),
+                origin_timeslot_start: Some((now + Duration::try_seconds(1).unwrap()).into()),
+                origin_timeslot_end: Some((now + Duration::try_seconds(2).unwrap()).into()),
+                target_timeslot_start: Some((now + Duration::try_seconds(3).unwrap()).into()),
+                target_timeslot_end: Some((now + Duration::try_seconds(4).unwrap()).into()),
+                path: Some(GeoLineStringZ {
+                    points: vec![GeoPointZ {
+                        y: 1.0,
+                        x: 2.0,
+                        z: 3.0,
+                    }],
+                }),
+                ..Default::default()
+            }),
+        };
+
+        let mut tmp = object.clone();
+        tmp.data = None;
+        assert_eq!(
+            FlightPlan::try_from(tmp).unwrap_err(),
+            FlightPlanError::Data
+        );
+
+        let mut tmp = object.clone();
+        tmp.data.as_mut().unwrap().origin_vertiport_id = None;
+        assert_eq!(
+            FlightPlan::try_from(tmp).unwrap_err(),
+            FlightPlanError::OriginVertiportId
+        );
+
+        let mut tmp = object.clone();
+        tmp.data.as_mut().unwrap().target_vertiport_id = None;
+        assert_eq!(
+            FlightPlan::try_from(tmp).unwrap_err(),
+            FlightPlanError::TargetVertiportId
+        );
+
+        let mut tmp = object.clone();
+        tmp.data.as_mut().unwrap().origin_timeslot_start = None;
+        assert_eq!(
+            FlightPlan::try_from(tmp).unwrap_err(),
+            FlightPlanError::OriginTimeslotStart
+        );
+
+        let mut tmp = object.clone();
+        tmp.data.as_mut().unwrap().origin_timeslot_end = None;
+        assert_eq!(
+            FlightPlan::try_from(tmp).unwrap_err(),
+            FlightPlanError::OriginTimeslotEnd
+        );
+
+        let mut tmp = object.clone();
+        tmp.data.as_mut().unwrap().target_timeslot_start = None;
+        assert_eq!(
+            FlightPlan::try_from(tmp).unwrap_err(),
+            FlightPlanError::TargetTimeslotStart
+        );
+
+        let mut tmp = object.clone();
+        tmp.data.as_mut().unwrap().target_timeslot_end = None;
+        assert_eq!(
+            FlightPlan::try_from(tmp).unwrap_err(),
+            FlightPlanError::TargetTimeslotEnd
+        );
+
+        let mut tmp = object.clone();
+        tmp.data.as_mut().unwrap().path = None;
+        assert_eq!(
+            FlightPlan::try_from(tmp).unwrap_err(),
+            FlightPlanError::Path
+        );
+    }
+
+    #[tokio::test]
+    async fn test_health_check() {
+        let config = crate::config::Config::default();
+        let grpc_clients = GrpcClients::default(config);
+        let result = health_check(Extension(grpc_clients)).await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_acknowledge_flight_plan() {
+        // bad request - invalid uuid
+        let payload = AckRequest {
+            fp_id: "invalid".to_string(),
+            status: AckStatus::Confirm,
+        };
+        let config = crate::config::Config::default();
+        let grpc_clients = GrpcClients::default(config);
+        let error = acknowledge_flight_plan(Extension(grpc_clients), Json(payload))
+            .await
+            .unwrap_err();
+        assert_eq!(error, StatusCode::BAD_REQUEST);
+    }
+
+    #[tokio::test]
+    async fn test_get_flight_plans() {
+        let aircraft_id = Bytes::from("invalid");
+        let config = crate::config::Config::default();
+        let grpc_clients = GrpcClients::default(config);
+        let error = get_flight_plans(Extension(grpc_clients.clone()), aircraft_id)
+            .await
+            .unwrap_err();
+        assert_eq!(error, StatusCode::BAD_REQUEST);
+
+        let aircraft_id = Bytes::from(vec![0xFF]); // non-UTF8 character
+        let error = get_flight_plans(Extension(grpc_clients.clone()), aircraft_id)
+            .await
+            .unwrap_err();
+        assert_eq!(error, StatusCode::BAD_REQUEST);
+
+        let aircraft_id = Bytes::from(Uuid::new_v4().to_string());
+        let results = get_flight_plans(Extension(grpc_clients.clone()), aircraft_id)
+            .await
+            .unwrap()
+            .0;
+        assert!(results.is_empty());
+
+        let data = vehicle::mock::get_data_obj();
+        let aircraft_id = grpc_clients
+            .storage
+            .vehicle
+            .insert(data)
+            .await
+            .unwrap()
+            .into_inner()
+            .object
+            .unwrap()
+            .id;
+
+        let results = get_flight_plans(
+            Extension(grpc_clients.clone()),
+            Bytes::from(aircraft_id.clone()),
+        )
+        .await
+        .unwrap()
+        .0;
+
+        assert!(results.is_empty());
+    }
+
+    #[test]
+    fn test_flight_plan_error_display() {
+        assert_eq!(
+            FlightPlanError::Data.to_string(),
+            "could not get data from object."
+        );
+        assert_eq!(
+            FlightPlanError::OriginVertiportId.to_string(),
+            "could not get origin_vertiport_id from data."
+        );
+        assert_eq!(
+            FlightPlanError::TargetVertiportId.to_string(),
+            "could not get target_vertiport_id from data."
+        );
+        assert_eq!(
+            FlightPlanError::OriginTimeslotStart.to_string(),
+            "could not get origin_timeslot_start from data."
+        );
+        assert_eq!(
+            FlightPlanError::OriginTimeslotEnd.to_string(),
+            "could not get origin_timeslot_end from data."
+        );
+        assert_eq!(
+            FlightPlanError::TargetTimeslotStart.to_string(),
+            "could not get target_timeslot_start from data."
+        );
+        assert_eq!(
+            FlightPlanError::TargetTimeslotEnd.to_string(),
+            "could not get target_timeslot_end from data."
+        );
+        assert_eq!(
+            FlightPlanError::Path.to_string(),
+            "could not get path from data."
+        );
     }
 }
